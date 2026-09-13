2463550bd394192b47b802aa5190c5aec62026cc
lubasinkal <lubasinkal@users.noreply.github.com>
2026-09-13 22:25:38 +0000

    Jobs index redirects to search, kills /jobs/ 404

---
+++ app/pages/jobs/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="wrap">
+    <p>opening search… <NuxtLink to="/">go now</NuxtLink></p>
+  </section>
+</template>
+
+<script setup lang="ts">
+const route = useRoute()
+await navigateTo({ path: '/', query: route.query }, { redirectCode: 301 })
+</script>
